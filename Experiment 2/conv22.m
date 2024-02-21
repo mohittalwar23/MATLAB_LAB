2c1d15c986e584012272c008f23de345ba95b5ed
Mohit <talwarmohit2005@gmail.com>
2024-02-21 14:01:15 +0530

    added exp-3

--- Experiment 2/conv22.m
+++ Experiment 2/conv22.m
@@ -2,12 +2,16 @@
 
 clear all
 clc
-
-n=-12:1:12;
+alpha=0.7;
+n=0:1:50;
+A=10;
+M=1;
+N=6; 
+type_of_sqn=4;
 len_n=length(n);
 
-h_n=[1 2 -1];
-g_n=[2 1 1 ];
+h_n=[1 -1];
+g_n=digital_sqn_gen(n,type_of_sqn, alpha, A, M, N);
 
 l1=length(h_n);
 l2=length(g_n);
